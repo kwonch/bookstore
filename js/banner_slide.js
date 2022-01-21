229ccdc27178226ca6f498b04a4c9890140052c1
kwonch <rnjsskfen153@naver.com>
2022-01-21 18:40:55 +0900

    2022-01-21
    banner 작업중 슬라이드 사이드 배너 하단 버튼 작업 완료
    배너 6번 넣어야힘

--- js/banner_slide.js
+++ js/banner_slide.js
@@ -3,16 +3,50 @@
 $.ajax({
     method: "GET",
     url: "https://dapi.kakao.com/v3/search/book?target=title",
-    data: { query:"아내의 시간" },
+    data: { query:"실격당한 자들을 위한 변론" },
     headers:{Authorization:"KakaoAK 9c0fe9e4d691d7756ab6ad4fc9524dee"}
   })
     .done(function( msg ) {
-      $(".book1").append("<img src='"+msg.documents[1].thumbnail+"'/>");
-      $(".book1").append("<h6 class='banner_book'>"+msg.documents[1].title+"</h6>")
+      $(".book1").append("<img src='"+msg.documents[0].thumbnail+"'/>");
+      $(".book1").append("<h6 class='banner_book'>"+msg.documents[0].title+"</h6>")
      
-      var str=msg.documents[1].contents;
-      var str2=str.substring(0,27);
+      var str=msg.documents[0].contents;
+      var str2=str.substring(0,65);
 
       $(".book1").append("<p class='banner_book'>"+str2+"</p>");
     
     });
+
+    $.ajax({
+        method: "GET",
+        url: "https://dapi.kakao.com/v3/search/book?target=title",
+        data: { query:"마음의 주인" },
+        headers:{Authorization:"KakaoAK 9c0fe9e4d691d7756ab6ad4fc9524dee"}
+      })
+        .done(function( msg ) {
+          $(".book2").append("<img src='"+msg.documents[0].thumbnail+"'/>");
+          $(".book2").append("<h6 class='banner_book'>"+msg.documents[0].title+"</h6>")
+         
+          var str=msg.documents[0].contents;
+          var str2=str.substring(0,65);
+    
+          $(".book2").append("<p class='banner_book'>"+str2+"</p>");
+        
+        });
+
+        $.ajax({
+            method: "GET",
+            url: "https://dapi.kakao.com/v3/search/book?target=title",
+            data: { query:"언어의 온도(170만부 기념 에디션)" },
+            headers:{Authorization:"KakaoAK 9c0fe9e4d691d7756ab6ad4fc9524dee"}
+          })
+            .done(function( msg ) {
+              $(".book3").append("<img src='"+msg.documents[0].thumbnail+"'/>");
+              $(".book3").append("<h6 class='banner_book'>"+msg.documents[0].title+"</h6>")
+             
+              var str=msg.documents[0].contents;
+              var str2=str.substring(0,65);
+        
+              $(".book3").append("<p class='banner_book'>"+str2+"</p>");
+            
+            });
