16166a85bb070d6187d4f9103a43952b4c9a813c
kwonch <rnjsskfen153@naver.com>
2022-02-04 15:57:46 +0900

    2022-02-04
    구매페이지 완성 밑에 슬라이드도 완성

--- sub_js/sub_category.js
+++ sub_js/sub_category.js
@@ -25,3 +25,47 @@ $(".category_title_4").mouseover(function () {
       $(" .category_bottom_4").stop().hide();
        });       
 });
+
+$(function () {
+    
+    // (input 숫자 증가)
+    $(".fa-plus-square").click(function () {
+        var i = $(this).siblings("input").val();
+        i++;
+        $(this).siblings("input").val(i);
+    });
+
+    // (input 숫자 감소)
+    $(".fa-minus-square").click(function () {
+        var i = $(this).siblings("input").val();
+        i--;
+        if (i <= 0) {
+            alert("최소 주문수량은 1개입니다.");
+            i = 1;
+        }
+        $(this).siblings("input").val(i);
+    });
+});
+
+$.ajax({
+    method: "GET",
+    url: "https://dapi.kakao.com/v3/search/book?target=title",
+    data: {
+        query: "불편한 편의점(15만부 기념 윈터 에디션)",
+        size: 6
+    },
+    headers: {
+        Authorization: "KakaoAK 9c0fe9e4d691d7756ab6ad4fc9524dee"
+    }
+})
+
+.done(function (msg){
+    for(var i=0; i<6; i++){
+       
+        $(".book_in_right_title_2 ").eq(i).append("<h2>"+msg.documents[i].title+"</h2>");
+        $(".price_1").eq(i).append("<span>"+msg.documents[i].price+"원</span>");
+        
+
+        
+    }
+});
---
+++ sub_js/sub_slide_ajax.js
@@ -0,0 +1,102 @@
+$.ajax({
+    method: "GET",
+    url: "https://dapi.kakao.com/v3/search/book?target=title",
+    data: {
+        query: "서로",
+        size: 6
+    },
+    headers: {
+        Authorization: "KakaoAK 9c0fe9e4d691d7756ab6ad4fc9524dee"
+    }
+})
+
+.done(function (msg){
+    for(var i=0; i<6; i++){
+        $(".slide_contents_1>div").eq(i).append('<a href="#">'+"<img src='"+msg.documents[i].thumbnail+"'/>"+"</a>");
+        $(".slide_contents_1>div").eq(i).append("<h5>"+'<a href="#">'+msg.documents[i].title+"</a>"+"</h5>");
+        $(".slide_contents_1>div").eq(i).append("<h6>"+msg.documents[i].price+"원</h6>");
+        
+
+        
+    }
+});
+
+$.ajax({
+    method: "GET",
+    url: "https://dapi.kakao.com/v3/search/book?target=title",
+    data: {
+        query: "서점",
+        size: 6
+    },
+    headers: {
+        Authorization: "KakaoAK 9c0fe9e4d691d7756ab6ad4fc9524dee"
+    }
+})
+
+.done(function (msg){
+    for(var i=0; i<6; i++){
+        $(".slide_contents_2>div").eq(i).append('<a href="#">'+"<img src='"+msg.documents[i].thumbnail+"'/>"+"</a>");
+        $(".slide_contents_2>div").eq(i).append("<h5>"+'<a href="#">'+msg.documents[i].title+"</a>"+"</h5>");
+        $(".slide_contents_2>div").eq(i).append("<h6>"+msg.documents[i].price+"원</h6>");
+        
+
+        
+    }
+});
+
+$.ajax({
+    method: "GET",
+    url: "https://dapi.kakao.com/v3/search/book?target=title",
+    data: {
+        query: "너의",
+        size: 6
+    },
+    headers: {
+        Authorization: "KakaoAK 9c0fe9e4d691d7756ab6ad4fc9524dee"
+    }
+})
+
+.done(function (msg){
+    for(var i=0; i<6; i++){
+        $(".slide_contents_3>div").eq(i).append('<a href="#">'+"<img src='"+msg.documents[i].thumbnail+"'/>"+"</a>");
+        $(".slide_contents_3>div").eq(i).append("<h5>"+'<a href="#">'+msg.documents[i].title+"</a>"+"</h5>");
+        $(".slide_contents_3>div").eq(i).append("<h6>"+msg.documents[i].price+"원</h6>");
+        
+
+        
+    }
+});
+
+
+
+$(function(){
+//  left(prev) show hide
+    var i = 0
+$(".book_in_bottom_slide>.fa-chevron-left").click(function () {
+
+    i--;
+
+    if (i < 0) {
+        i = 2
+    }
+
+    $(".book_in_slide_wrap").stop().hide();
+    $(".book_in_slide_wrap").eq(i).stop().show();
+
+});
+
+    // right(next) show hiede
+var i = 0
+$(".book_in_bottom_slide>.fa-chevron-right").click(function () {
+
+    i++;
+
+    if (i > 2) {
+        i = 0
+    }
+
+    $(".book_in_slide_wrap").stop().hide();
+    $(".book_in_slide_wrap").eq(i).stop().show();
+});
+
+});
