dd5803c22da09f27e795ff393f938c55bc0c341f
kwonch <rnjsskfen153@naver.com>
2022-02-06 22:30:01 +0900

    Update sub_get_text_book_infor.js

--- sub_js/sub_get_text_book_infor.js
+++ sub_js/sub_get_text_book_infor.js
@@ -1,12 +1,12 @@
 $(function(){
-        $.get("./sub_text/sub_text_1.txt",function(data){
+        $.get("/sub_text/sub_text_1.txt",function(data){
            $(".book_infor_3").html(data);
         });
-        $.get("./sub_text/sub_text_2.txt",function(data){
+        $.get("/sub_text/sub_text_2.txt",function(data){
             $(".book_infor_4").html(data);
          });
        
-         $.get("./sub_text/sub_authors2.txt",function(data){
+         $.get("/sub_text/sub_authors2.txt",function(data){
             $(".author_text_2").html(data);
          });
     });
